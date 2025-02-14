d8a1c3cb2b4ffd8d4b0184056f06eb111c5f57a6
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2025-02-14 15:13:37 +0530

    Create FindDuplicates.java

---
+++ FindDuplicates.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i: nums){
+            if(map.containsKey(i)){
+                int count = map.get(i);
+                map.put(i,count+1);
+            }else{
+                map.put(i,1);
+            }}
+        List<Integer> duplicates = new ArrayList<>(); 
+        for(int i: map.keySet()){
+            if(map.get(i) == 2){
+                duplicates.add(i);
+            }
+        }
+        return duplicates;
+    
+
+    }
+}
