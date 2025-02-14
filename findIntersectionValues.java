87352ec241f33bf2868626232128ad6fbd6b952d
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2025-02-14 13:04:29 +0530

    Create findIntersectionValues.java

---
+++ findIntersectionValues.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for(int i: nums1){
+            set1.add(i);
+        }
+
+        for(int i: nums2){
+            set2.add(i);
+        }
+
+        int count1 = 0;
+        for(int i : nums1){
+            if(set2.contains(i)){
+                count1++;
+            }
+        }
+
+        int count2 = 0;
+        for(int i : nums2){
+            if(set1.contains(i)){
+                count2++;
+            }
+        }
+
+        return new int[]{count1,count2};
+
+        
+    }
+}
